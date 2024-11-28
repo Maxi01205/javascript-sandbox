58fdab66b6630e14fad1c446d0391bd1c7a6ec78
Maxi01205 <175952649+Maxi01205@users.noreply.github.com>
2024-11-28 08:42:41 +0100

    fist upload

---
+++ javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -0,0 +1,17 @@
+console.log(100);
+
+console.log("Hello World");
+
+console.log(20, "Hello", true);
+
+const x = 100;
+
+console.log(x);
+
+console.error("Allert");
+
+console.warn("Warning");
+
+const styles = "padding: 10px; background-color: white; color: green;";
+
+console.log("%cHello World", styles);
